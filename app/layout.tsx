526897c2d637363cf9d4331e521e42392f39e27c
Ridho Tamma <ridho.tamma@gmail.com>
2023-08-14 09:44:17 +0700

    update: remove unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,15 +8,12 @@ import { SidebarProvider } from "@/contexts/SidebarContext";
 import Footer from "@/components/shared/Footer";
 import Sidebar from "@/components/shared/Sidebar";
 import Navbar from "@/components/shared/Navbar";
-import { useToast } from "@/components/toast";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function RootLayout({ children }: Props) {
-  const toast = useToast()
-
   return (
     <html lang="en">
       <body>
@@ -28,7 +25,6 @@ export default function RootLayout({ children }: Props) {
           </main>
           <Footer />
         </SidebarProvider>
-        { toast.ToastPortal() }
       </body>
     </html>
   );
